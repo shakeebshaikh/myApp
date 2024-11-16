ee9b619e7f8e833d5af1a1da814ccd4fa64b744b
shakeebshaikh <shakeebshaikh98@gmail.com>
2024-11-17 02:36:55 +0530

    form validation added

--- lib/Screens/AddProductScreen.dart
+++ lib/Screens/AddProductScreen.dart
@@ -9,10 +9,32 @@ class AddProductScreen extends StatefulWidget {
 
 class _AddProductScreenState extends State<AddProductScreen> {
   final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+  final TextEditingController _offerPriceController = TextEditingController();
+  final TextEditingController _quantityController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
   static const formLableStyle = TextStyle(
     fontWeight: FontWeight.bold,
     fontSize: 18,
   );
+
+
+
+  void _saveProduct() {
+   // Check if the form is valid
+    if (_formKey.currentState!.validate()) {
+      // Perform form submission actions (e.g., save data, navigate)
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Form Submitted Successfully!')),
+      );
+    } else {
+      // Show an error message
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please fill all required fields.')),
+      );
+    }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -34,13 +56,27 @@ class _AddProductScreenState extends State<AddProductScreen> {
                     style: formLableStyle,
                   ),
                   Container(
-                    color: Colors.grey[400],
+                    decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 45, 45, 45)
+                      ),
+                      borderRadius: BorderRadius.circular(8)
+                    ),
+                    
                     height: 80,
                     width: 80,
                     child: const Icon(Icons.add_a_photo),
                   ),
                   const SizedBox(height: 14,),
                   TextFormField(
+                   validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Product name is required';
+                  }
+                  return null;
+                },
+              controller: _nameController,
                     decoration: const InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: 'Enter Product Name',
@@ -48,6 +84,16 @@ class _AddProductScreenState extends State<AddProductScreen> {
                   ),
                   const SizedBox(height: 14,),
                   TextFormField(
+                    validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Price is required';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Enter a valid number';
+                  }
+                  return null;
+                },
+                    controller: _priceController,
                     keyboardType: TextInputType.number,
                     decoration: const InputDecoration(
                         border: OutlineInputBorder(),
@@ -56,6 +102,16 @@ class _AddProductScreenState extends State<AddProductScreen> {
                   ),
                   const SizedBox(height: 14,),
                   TextFormField(
+                    validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Offer Price is required';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Enter a valid number';
+                  }
+                  return null;
+                },
+                    controller: _offerPriceController,
                     keyboardType: TextInputType.number,
                     decoration: const InputDecoration(
                         border: OutlineInputBorder(),
@@ -64,6 +120,16 @@ class _AddProductScreenState extends State<AddProductScreen> {
                   ),
                   const SizedBox(height: 14,),
                   TextFormField(
+                    validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Quantity is required';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Enter a valid number';
+                  }
+                  return null;
+                },
+                    controller: _quantityController,
                     keyboardType: TextInputType.number,
                     decoration: const InputDecoration(
                         border: OutlineInputBorder(),
@@ -72,6 +138,13 @@ class _AddProductScreenState extends State<AddProductScreen> {
                   ),
                   const SizedBox(height: 14,),
                   TextFormField(
+                    validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Product name is required';
+                  }
+                  return null;
+                },
+                    controller: _descriptionController,
                     decoration: const InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: 'Enter Product Description',
@@ -79,7 +152,10 @@ class _AddProductScreenState extends State<AddProductScreen> {
                   ),
                   const SizedBox(height: 14,),
                   Center(
-                    child: ElevatedButton(onPressed: (){}, 
+                    child: ElevatedButton(
+                      onPressed: (){
+                        _saveProduct();
+                      }, 
                     child:const Text('Save Product')),
                   )
                 ],
@@ -91,3 +167,5 @@ class _AddProductScreenState extends State<AddProductScreen> {
     );
   }
 }
+
+
