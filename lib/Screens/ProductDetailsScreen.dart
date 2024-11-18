7c17cd12627d86933bdbc4fcbda512c27c3d4cc0
shakeebshaikh <shakeebshaikh98@gmail.com>
2024-11-18 23:25:30 +0530

    add to cart button added on pro details screen

--- lib/Screens/ProductDetailsScreen.dart
+++ lib/Screens/ProductDetailsScreen.dart
@@ -11,30 +11,68 @@ class ProductDetailsScreen extends StatefulWidget {
 }
 
 class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  final TextStyle labelStyle =
+      const TextStyle(fontWeight: FontWeight.bold, fontSize: 18);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.product!.name),
-      ),
-      body:Container(
-        margin: const EdgeInsets.all(16),
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Image.file(widget.product!.imagePath!,
-              width: MediaQuery.sizeOf(context).width / 1.2,),
-            ),
-            const Divider(),
-            Text(widget.product!.name),
-            Text(widget.product!.price.toString()),
-            Text(widget.product!.offerPrice.toString()),
-            Text(widget.product!.description),
-          ],
+        appBar: AppBar(
+          title: Text(widget.product!.name),
         ),
-      )
-    );
+        body: Container(
+          margin: const EdgeInsets.all(16),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.file(
+                  widget.product!.imagePath!,
+                  width: MediaQuery.sizeOf(context).width / 1.1,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const Divider(
+                color: Colors.black,
+                thickness: 2,
+                indent: 2,
+                endIndent: 2,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                'Name : ${widget.product!.name}',
+                style: labelStyle,
+              ),
+              Text(
+                'Price : ${widget.product!.price.toString()}',
+                style: labelStyle,
+              ),
+              Text(
+                'Offer Price : ${widget.product!.offerPrice.toString()}',
+                style: labelStyle,
+              ),
+              Text(
+                'Description : ${widget.product!.description}',
+                style: labelStyle,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: widget.product!.quantity >0 ? () {} : null,
+                  child: const Text(
+                    'Add to cart',
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
